2e4ce9bd10128e2ff0a3ba09b0c40c9fe7a389fe
pangiiz-qq <phebe.phiphi@gmail.com>
2025-07-18 15:37:51 +0700

    submit loop assignment

--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,8 @@
 let companyName = "TechUp";
 
 // Start coding here
+
+for (let i = 0; i < companyName.length; i++) {
+  let j = i + 1;
+  console.log("Number " + j + " character is " + companyName[i]);
+}
--- ex-6.js
+++ ex-6.js
@@ -4,4 +4,8 @@ let reversedCompanyName = "";
 
 // Start coding here
 
+for (let i = companyName.length; i > 0; i--) {
+  let j = i - 1;
+  reversedCompanyName += companyName[j];
+}
 console.log(reversedCompanyName);
